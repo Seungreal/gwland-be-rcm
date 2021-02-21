ea483de7e797164feffea5fcf5215e4f7dc9177f
SUHOPARK1 <qkrtnghsla@naver.com>
2021-02-21 18:21:32 +0900

    crawling test success

--- src/main/java/com/tplus/gwland/bbs/domain/BorardCrawling.java
+++ src/main/java/com/tplus/gwland/bbs/domain/BorardCrawling.java
@@ -1,20 +1,78 @@
 package com.tplus.gwland.bbs.domain;
 
-import java.io.IOException;
-
-
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+
+ 
+
 public class BorardCrawling {
-	public static void main(String[] args) throws IOException {
-		Document doc = Jsoup.connect("https://m.place.naver.com/my/review/6004dfbc6f7dc20061207174?v=2").get();
-		Elements el = doc.getElementsByAttributeValue("class","LinesEllipsis LinesEllipsis--clamped ");
-		Elements el2 = doc.getElementsByAttributeValue("class","CVsLec8cBK");
-		
-		System.out.println(el);
-		System.out.println(el2);
-		System.out.println("aaa");
-	}
+
+public static void main(String args[]) {
+
+/*
+
+ * Document 클래스 : 연결해서 얻어온 HTML 전체 문서
+
+ * Element 클래스 : Document의 HTML 요소
+
+ * Elements 클래스 : Element가 모인 자료형
+
+ */
+
+ 
+
+String url ="https://sports.news.naver.com/wfootball/index.nhn";
+
+Document doc = null;
+
+ 
+
+try {
+
+doc = Jsoup.connect(url).get();
+
+} catch (Exception e) {
+
+// TODO: handle exception
+
+e.printStackTrace();
+
 }
+
+ 
+
+//주요 뉴스 태그
+
+Elements element = doc.select("div.home_news");
+
+//헤더부분의 제목
+
+String title = element.select("h2").text().substring(0,4);
+
+ 
+
+System.out.println("=======================================================");
+
+System.out.println(title);
+
+System.out.println("=======================================================");
+
+ 
+
+for(Element el : element.select("li")) { //하위 뉴스 기사들을 for문 돌리며 출력
+
+System.out.println(el.text());
+
+}
+
+ 
+
+System.out.println("=======================================================");
+
+}
+
+}
+
--- src/main/java/com/tplus/gwland/rat/domain/Rating.java
+++ src/main/java/com/tplus/gwland/rat/domain/Rating.java
@@ -1,5 +1,10 @@
 package com.tplus.gwland.rat.domain;
 
+import lombok.Data;
+
+@Data
 public class Rating {
+	public  int ratingNum;
+	public float rationStar;
 
 }
