e60e8a5f82f1511dd0ae58f07798a30c77209f2a
SUHOPARK1 <qkrtnghsla@naver.com>
2021-03-11 11:38:16 +0900

    Crawler -> .csv 구현

---
+++ src/main/java/com/tplus/gwland/cmm/util/MakeCsv.java
@@ -0,0 +1,19 @@
+package com.tplus.gwland.cmm.util;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.util.List;
+
+public class MakeCsv {
+	public static int createCSV(List<reviewer> list, String title, String filepath) {
+		int resultCount = 0;
+		try{
+			BufferedWriter fw = new BufferedWriter(new FileWriter(filepath + "/" + title + ".csv", true));
+			for(reviewer dom:list) {fw.write(dom.getPceNumr()+","+dom.getRevNick()+","+ 
+											 dom.getRevContent()+","+dom.getRevDate());
+			fw.newLine();}
+			fw.flush();
+			fw.close();
+			}catch (Exception e) {
+				e.printStackTrace();}
+			return resultCount;}}
