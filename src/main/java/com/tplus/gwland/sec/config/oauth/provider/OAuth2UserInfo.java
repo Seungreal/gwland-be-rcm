3edaf87adbdffe2478f4a609fa89d4871b5f941b
SUHOPARK1 <qkrtnghsla@naver.com>
2021-03-19 20:06:52 +0900

    gitignore test

--- src/main/java/com/tplus/gwland/sec/config/oauth/provider/OAuth2UserInfo.java
+++ src/main/java/com/tplus/gwland/sec/config/oauth/provider/OAuth2UserInfo.java
@@ -2,10 +2,14 @@ package com.tplus.gwland.sec.config.oauth.provider;
 
 public interface OAuth2UserInfo {
 	String getProviderId();
+
 	String getProvider();
+
 	String getEmail();
+
 	String getName();
+
 	String getAge();
+
 	String getGender();
 }
-
