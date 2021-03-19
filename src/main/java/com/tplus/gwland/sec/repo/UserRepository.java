3e97ed0ee84122b4eb141a19416ee87ff233d95e
SUHOPARK1 <qkrtnghsla@naver.com>
2021-03-19 20:49:37 +0900

    1

--- src/main/java/com/tplus/gwland/sec/repo/UserRepository.java
+++ src/main/java/com/tplus/gwland/sec/repo/UserRepository.java
@@ -12,7 +12,6 @@ import com.tplus.gwland.sec.model.User;
  * JPA는 기본 CRUD를 JpaRepository가 상속하는 CrudRepository가 가지고 있음.
  * JPA는 method names 전략을 가짐. README.md 사진 참고
  */
-
 // JpaRepository 를 상속하면 자동 컴포넌트 스캔됨.
 public interface UserRepository extends JpaRepository<User, Integer>{
 	// SELECT * FROM user WHERE username = ?1
